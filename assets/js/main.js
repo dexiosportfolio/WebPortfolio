da41e48ea77a0fae40ff307d8615a76e2f6eba2f
dexiosportfolio <155410949+dexiosportfolio@users.noreply.github.com>
2024-01-02 18:07:36 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 100
+  });
+  
+  ScrollReveal().reveal('h1, h4, h6, .service, .card-custom, .bg-base, .col-12, h5', { origin:'bottom' });
+
+
+  ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 200
+  });
+  
+  ScrollReveal().reveal('p', { origin:'bottom' });
+
+  ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 300
+  });
+  
+  ScrollReveal().reveal('.btn', { origin:'bottom' });
